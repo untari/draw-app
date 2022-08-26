6d00342f5c850392cd7ca0b36dbefb36a70c2d66
untari <sastraembun0306@gmail.com>
2022-08-26 13:15:27 +0800

    changed the icon images

--- stampTool.js
+++ stampTool.js
@@ -6,10 +6,20 @@ function StampTool(){
     var star = loadImage('assets/star.jpg');
     var starSize = 50;
 
-    this.draw = function(){
+    //create variable for slider
+    var starSizeSlider;
 
+    starSizeSlider = createSlider(5, 50, 20);
+    starSizeSlider.parent("#sizeOfStarControl");
+
+    this.draw = function(){
+        
         if(mouseIsPressed){
-            image(star, mouseX, mouseY, starSize, starSize);
+            //create new variables for when mouse is pressed 
+            var starX = mouseX - starSize/2;
+            var starY = mouseY - starSize/2;
+            //change mouseX and mouseY variables into starX and starY 
+            image(star, starX, starY, starSize, starSize);
         }
     }
 
